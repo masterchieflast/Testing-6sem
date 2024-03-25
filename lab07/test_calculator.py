fe45f4409f4ab702e87f50f05bc5071dff298332
masterchieflast <denis16.05.2004@icloud.com>
2024-03-25 22:41:54 +0300

    lab07

--- lab07/test_calculator.py
+++ lab07/test_calculator.py
@@ -15,16 +15,19 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.subtract(5, 3), 2)
         self.assertEqual(self.calculator.subtract(-1, 1), -2)
         self.assertEqual(self.calculator.subtract(0, 0), 0)
+        self.assertEqual(self.calculator.subtract(4, 3), 1)
 
     def test_multiply(self):
         self.assertEqual(self.calculator.multiply(2, 3), 6)
         self.assertEqual(self.calculator.multiply(-1, 5), -5)
         self.assertEqual(self.calculator.multiply(0, 0), 0)
+        self.assertEqual(self.calculator.multiply(0, 1231), 0)
 
     def test_divide(self):
         self.assertEqual(self.calculator.divide(8, 2), 4)
         self.assertEqual(self.calculator.divide(-6, 3), -2)
         self.assertEqual(self.calculator.divide(0, 5), 0)
+        self.assertEqual(self.calculator.divide(3, 1), 3)
         with self.assertRaises(ValueError):
             self.calculator.divide(5, 0)
 
