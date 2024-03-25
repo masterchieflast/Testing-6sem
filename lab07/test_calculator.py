9a1e651d97f727d98763855e9815bb10f184b722
masterchieflast <denis16.05.2004@icloud.com>
2024-03-25 22:38:33 +0300

    lab07

--- lab07/test_calculator.py
+++ lab07/test_calculator.py
@@ -1,26 +1,8 @@
 import unittest
-class Calculator:
-    @staticmethod
-    def add(x, y):
-        return x + y
-
-    @staticmethod
-    def subtract(x, y):
-        return x - y
-
-    @staticmethod
-    def multiply(x, y):
-        return x * y
-
-    @staticmethod
-    def divide(x, y):
-        if y == 0:
-            raise ValueError("Cannot divide by zero")
-        return x / y
+from calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
-
     def setUp(self):
         self.calculator = Calculator()
 
